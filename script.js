521ec422ad78fdf44a0ad5393643a5692a1b50e6
arthurktripp <arthur.k.tripp@gmail.com>
2020-09-08 15:45:16 -0700

    basic 16x16 grid with mouseover

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
- 
+var dimension = 16;
+var totalSquares = dimension * dimension;
+var i = 0;
+
+const container = document.querySelector("#container");
+
+
+
+function createPixels() {
+    for ( i = 0; i < totalSquares; i++ ) {
+        const pixel = document.createElement("div");
+        pixel.classList.add("blank");
+        pixel.setAttribute("id", i);
+        pixel.addEventListener("mouseover", () => {
+            pixel.classList.remove("blank");
+            pixel.classList.add("dark");
+        })
+
+        container.appendChild(pixel);
+    }
+}
+
+createPixels();
